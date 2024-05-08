bd5359baa79c3d8e74ef410a6e038e599a2e6183
Momin Khan <mominkhan.dev@gmail.com>
2024-05-08 19:07:11 +0500

    feat: tag-details page

---
+++ app/(root)/tags/[id]/page.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { getQuestionsByTagId } from "@/lib/actions/tags.action";
+import LocalSearchbar from "@/components/shared/search/LocalSearch";
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import { QuestionType } from "@/types";
+import { capitalizeWord } from "@/helpers/sanitizer";
+
+const Page = async ({ params }: any) => {
+  const tagQuestions = await getQuestionsByTagId(params.id);
+
+  const { name, questions } = tagQuestions;
+
+  return (
+    <main>
+      <h1 className="h1-bold text-dark100_light900">{capitalizeWord(name)}</h1>
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search tag questions"
+          otherClasses="flex-1"
+        />
+      </div>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions?.length > 0 ? (
+          questions.map((question: QuestionType) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="No Tags Found"
+            description="It looks like there are no tags found."
+            link="/ask-question"
+            linkTitle="Ask a question"
+          />
+        )}
+      </div>
+    </main>
+  );
+};
+export default Page;
