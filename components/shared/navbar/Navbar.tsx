d256c671118dc260726bcb84fd2a641943a34ef2
Momin Khan <mominkhan.dev@gmail.com>
2024-06-19 03:51:58 +0500

    remove theme dropdown

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const { sessionId } = auth();
 
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 px-6 py-3.5 shadow-light-300 dark:shadow-none sm:px-12 bg-blue-400">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 px-6 py-3.5 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
